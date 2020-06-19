76abd0156dae7aa4ec9398adc9dfcc0d8db92d35
Tharshi <you@example.com>
2020-06-19 07:40:12 +0530

    first commit

--- src/components/layouts/publicLayout.js
+++ src/components/layouts/publicLayout.js
@@ -1,42 +1,71 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(( theme=theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
+import React from "react";
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+​
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    height: '100vh',
+  },
+  image: {
+    [theme.breakpoints.up("md")]: {
+      display: 'block',
     },
-    main: {
-      padding: theme.spacing(19),
-      textAlign: 'center',
-      color: theme.palette.secoundry,
-      alignItems:'center'
+    display: 'none',
+    backgroundColor: "rgba(253, 253, 253, 1)",
+  },
+  paper: {
+​
+    [theme.breakpoints.up("sm")]: {
+      width: "60%",
     },
-    paragraph:{
-      font: 'Regular 15px/19px PT Sans',
-      letterSpacing:' 0px',
-      color:' #181D33',
-      opacity: '0.48'
-    }
-  }),
-);
-
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: "75%",
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%'
+  },
+  img: {
+    paddingBottom: '25px'
+  }
+}));
+​
 function PublicLayout(props) {
-    const classes = useStyles(props);
-
-    return (
-        <Grid xs={12} container  >
-            <Grid item xs={6} className={classes.main} >
-              <img src={props.logo} alt="noda" />
-              <h1> {props.header} </h1>
-              <p className={classes.paragraph} >{props.paragraph} </p>
-            </Grid>
-            <Grid item xs={6} style={{border:' 0.25px solid #EDEDED'}} justify='center'>
-              {props.children}
-            </Grid>
-        </Grid>
-           )
+  const classes = useStyles(props);
+​
+  return (
+    <Grid item xs={12} classes={{
+      root: classes.root
+    }}>
+​
+​
+      <Grid item sm={false} md={6} className={classes.image}>
+        <div className={classes.container}>
+          <Box align="center" px={10}>
+            <img alt={props.title} src={props.image} className={classes.img}/>
+            <Typography variant="h5">{props.title}</Typography>
+            <Typography variant="subtitle1">{props.text}</Typography>
+          </Box>
+        </div>
+      </Grid>
+​
+​
+​
+      <Grid item xs={6} className={classes.container}>{props.children}
+      </Grid>
+    </Grid>
+  );
 }
-
+​
 export default PublicLayout;
+
--- src/routes/publicroutes.js
+++ src/routes/publicroutes.js
@@ -1,15 +1,12 @@
-import signup from '../pages/signup';
-import Login from '../pages/login';
-import Profile from '../pages/profile';
-import Complete from'../pages/completed';
-import uncomplete from'../pages/uncompleted';
-import Forgetpass from'../pages/forgetpass';
+import Welcome from '../pages/Welcome';
+import Login from '../pages/Login';
+
 
 
 const publicRoutes = [
     {
-        path:"/signup",
-        component:signup,
+        path:"/welcome",
+        component:Welcome,
         exact:true
     },
     {
@@ -17,26 +14,7 @@ const publicRoutes = [
         component:Login,
         exact:true
     },
-    {
-        path:"/profile",
-        component:Profile,
-        exact:true
-    },
-    {
-        path:"/complete",
-        component:Complete,
-        exact:true
-    },
-    {
-        path:"/uncomplete",
-        component:uncomplete,
-        exact:true
-    },
-    {
-        path:"/forgetpass",
-        component:Forgetpass,
-        exact:true
-    },
+
 
 
 ]
