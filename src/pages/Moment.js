6d288ce2155f684d93c880d539e4200c9525c5f5
Tharshika Balachandran <58292816+tharshi1123@users.noreply.github.com>
2020-06-30 07:41:29 +0530

    Add files via upload
    Moment commit

---
+++ src/pages/Moment.js
@@ -0,0 +1,41 @@
+import moment from 'moment';
+import React, { Component, PropTypes } from 'react';
+
+export default class Moment extends Component {
+  static propTypes = {
+};
+
+  static defaultProps = {
+    format: 'MMMM Do YYYY, h:mm a',
+    liveUpdate: false,
+  };
+
+  componentDidMount() {
+    const { liveUpdate, liveUpdateInterval } = this.props;
+
+    if (liveUpdate || liveUpdateInterval) {
+      const interval = liveUpdateInterval || 10000;
+      this.interval = setInterval(() => this.forceUpdate(), interval);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  render() {
+    const { date, children, format, ...rest } = this.props;
+
+    return (
+      <div {...rest}>
+        {children ?
+            children(moment(date))
+          :
+            moment(date).format(format)
+        }
+      </div>
+    );
+  }
+}
