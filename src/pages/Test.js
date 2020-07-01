9e7acc209140952186979c574dc0aefc7972feb5
Tharshika Balachandran <58292816+tharshi1123@users.noreply.github.com>
2020-07-01 08:15:03 +0530

    Update Test.js

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -3,6 +3,8 @@ import PublicLayout from '../components/layouts/publicLayout';
 import signinImage from '../assets/images/Signin.png';
 import { withStyles } from '@material-ui/styles';
 import Buttons from '../components/core/Button';
+import InputField from '../components/core/Input';
+import Grid from '@material-ui/core/Grid';
 
 const styles = (theme) => ({
   logo: {
@@ -49,11 +51,27 @@ this.setState({emailid:this.state.emailid.concat(newEmail),});
         text='Large volumes of diverse data systematically categorized based on distinct criteria for easy and effective filtering of the necessary information.'
       >
     <div>
+    <form className={classes.formCart}>
+      <Grid container item xs={12} sm={12} md={12} lg={12}>
+        <InputField
+          id={'email'}
+          name={'email'}
+          type={'email'}
+          label={'Email address'}
+          placeholder={'Enter email address'}
+         /* error={Boolean(formik.errors.email) && formik.touched.email}
+          errorMessage={formik.errors.email}
+          value={formik.values.email}
+          getValue={(vaule) => formik.setFieldValue('email', vaule)}
+          onBlur={() => formik.setFieldTouched('email')}*/
+        />
+      </Grid>
       <ul>{this.state.emailid.map((value,index) => <li key={ index }>{ value }</li>)}
      </ul>
     <Buttons color='primary' onClick={ this.addNewEmail }>Submit</Buttons>
+    </form>
     </div>
-   </PublicLayout>
+    </PublicLayout>
     );
   }
 }
