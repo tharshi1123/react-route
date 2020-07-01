330375e2f2c8b74879f28d82486afe8ae5359e45
Tharshika Balachandran <58292816+tharshi1123@users.noreply.github.com>
2020-07-01 07:43:09 +0530

    Update Test.js

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PublicLayout from '../components/layouts/publicLayout';
 import signinImage from '../assets/images/Signin.png';
-import logo from '../assets/logos/Logo.png';
-import LoginForm from '../components/forms/LoginForm';
 import { withStyles } from '@material-ui/styles';
 import Buttons from '../components/core/Button';
 
@@ -45,21 +43,20 @@ this.setState({emailid:this.state.emailid.concat(newEmail),});
     const { initialValues } = this.state;
 
     return (
-      <PublicLayout
+    <PublicLayout
         image={signinImage}
-        title="Standard Catalogued Data"
-        text="Large volumes of diverse data systematically categorized based on distinct criteria for easy and effective filtering of the necessary information."
+        title='Standard Catalogued Data'
+        text='Large volumes of diverse data systematically categorized based on distinct criteria for easy and effective filtering of the necessary information.'
       >
-
     <div>
       <ul>{this.state.emailid.map((value,index) => <li key={ index }>{ value }</li>)}
-    </ul>
+     </ul>
     <Buttons color='primary' onClick={ this.addNewEmail }>Submit</Buttons>
     </div>
-
-      </PublicLayout>
+   </PublicLayout>
     );
   }
 }
 
 export default withStyles(styles)(Login);
+
