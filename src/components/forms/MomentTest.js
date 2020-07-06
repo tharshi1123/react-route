71063ab17f144d6c643400a3db487c601ac380c2
Tharshika Balachandran <58292816+tharshi1123@users.noreply.github.com>
2020-07-06 22:10:00 +0530

    Add files via upload
    Moment.js second commit

--- src/components/forms/MomentTest.js
+++ src/components/forms/MomentTest.js
@@ -3,24 +3,34 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import { DateTimePicker } from "@material-ui/pickers";
-import { IconButton} from "@material-ui/core";
-import MoreIcon from "@material-ui/icons/MoreVert";
-
+import Buttons from '../core/Button';
+import moment from 'moment';
 
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import { date } from 'yup';
 
 export default function MaterialUIPickers() {
-  // The first commit of Material-UI
-  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+ 
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const [selectedDate2, setSelectedDate2] = React.useState(new Date());
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
+  const handleDateChange2 = (date) => {
+    setSelectedDate2(date);
+  };
+
+  const calculate =() => {
+    console.log(`Difference is ${moment(selectedDate2).diff(moment(selectedDate), 'days')}`
+    );
+  };
+  
 return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Grid container justify="space-around">
@@ -29,9 +39,16 @@ return (
           inputVariant="outlined"
           value={selectedDate}
           onChange={handleDateChange}
+          format="yyyy/MM/dd HH:mm"
          />
-         
-       
+         <DateTimePicker
+          label="DateTimePicker"
+          inputVariant="outlined"
+          value={selectedDate2}
+          onChange={handleDateChange2}
+          format="yyyy/MM/dd HH:mm"
+         /> 
+       <Buttons color="primary" onClick={calculate}>Calculate interval</Buttons>
       </Grid>
     </MuiPickersUtilsProvider>
   );
