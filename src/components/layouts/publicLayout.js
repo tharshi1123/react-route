fc6a43bd7c54d8db7d6708c60ba43cf0d99c7b97
Tharshika Balachandran <58292816+tharshi1123@users.noreply.github.com>
2020-07-10 13:20:50 +0530

    Add files via upload

--- src/components/layouts/publicLayout.js
+++ src/components/layouts/publicLayout.js
@@ -1,9 +1,10 @@
-import React from "react";
-import {makeStyles} from '@material-ui/core/styles';
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
-
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { createMuiTheme, ThemeProvider, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,46 +12,49 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
   },
   image: {
-    [theme.breakpoints.up("md")]: {
+    [theme.breakpoints.up('sm')]: {
       display: 'block',
     },
     display: 'none',
-    backgroundColor: "rgba(253, 253, 253, 1)",
+    backgroundColor: 'rgba(253, 253, 253, 1)',
   },
   paper: {
-
-    [theme.breakpoints.up("sm")]: {
-      width: "60%",
+    [theme.breakpoints.up('sm')]: {
+      width: '60%',
     },
     margin: theme.spacing(8, 4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    width: "75%",
+    width: '75%',
     marginLeft: 'auto',
-    marginRight: 'auto'
+    marginRight: 'auto',
   },
   container: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    height: '100%'
+    height: '100%',
   },
   img: {
-    paddingBottom: '25px'
-  }
+    paddingBottom: '25px',
+  },
 }));
 
 function PublicLayout(props) {
   const classes = useStyles(props);
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
 
   return (
-    <Grid item xs={12} classes={{
-      root: classes.root
-    }}>
-​
-​
-      <Grid item sm={false} md={6} className={classes.image}>
+    <Grid
+      item
+      xs={12}
+      classes={{
+        root: classes.root,
+      }}
+    >
+      {matches && <Grid item sm={12} md={6} >
         <div className={classes.container}>
           <Box align="center" px={10}>
             <img alt={props.title} src={props.image} className={classes.img}/>
@@ -58,15 +62,12 @@ function PublicLayout(props) {
             <Typography variant="subtitle1">{props.text}</Typography>
           </Box>
         </div>
-      </Grid>
-​
-​
-​
-      <Grid item xs={6} className={classes.container}>{props.children}
+      </Grid>}
+      <Grid item xs={12}  md={6} className={classes.container}>
+        {props.children}
       </Grid>
     </Grid>
   );
 }
 
 export default PublicLayout;
-
